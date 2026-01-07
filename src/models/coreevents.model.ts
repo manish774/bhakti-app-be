1083b0d3d03dadf44191cc427799e7c7b6076869
Manish774 <pnka3196@gmail.com>
2026-01-07 20:58:56 +0530

    fixed minor issue

--- src/models/coreevents.model.ts
+++ src/models/coreevents.model.ts
@@ -1,15 +1,28 @@
 import { model, Schema } from "mongoose";
 
+enum CoreEventIds {
+  ONLINE_PUJA = "coreevent_online_puja",
+  OFFLINE_PUJA = "coreevent_offline_puja",
+}
+
 //core
 interface CoreEvent {
   name: string;
   description?: string;
   status: "active" | "inactive";
   iconUrl?: string;
+  id: CoreEventIds;
 }
 
 const coreEventSchema = new Schema(
   {
+    id: {
+      type: String,
+      enum: Object.values(CoreEventIds),
+      required: true,
+      unique: true,
+      index: true,
+    },
     name: { type: String, required: true, unique: true, index: true },
     description: { type: String, required: false },
     status: {
